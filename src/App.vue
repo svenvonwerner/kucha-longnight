f0b629dbc2e657f765acb923fbf2d7dd98b4faa3
Sven von Werner <sven.von.werner@gmail.com>
2023-05-10 14:32:55 +0200

    openseadragon

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
   <v-app>
-    <v-container fluid class="bg-grey-lighten-1 fill-height pa-4">
-      <v-row no-gutters class="fill-height">
-        <v-col cols="2" class="bg-grey-lighten-4">
-          <v-btn>Button</v-btn>
-          <v-combobox
-            label="Combobox"
-            :items="['One', 'Two', 'Three']"
-          ></v-combobox>
-        </v-col>
-        <v-col cols="10" class="bg-grey-lighten-3">
-          <div id="openseadragon">Content</div>
-        </v-col>
-      </v-row>
-    </v-container>
+    <Game></Game>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Game from "./components/Game.vue";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld,
+    Game,
   },
 
   data: () => ({
---
+++ src/components/Game.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container fluid class="bg-grey-lighten-1 fill-height pa-4">
+    <v-row no-gutters class="fill-height">
+      <v-col cols="4" class="bg-grey-lighten-4">
+        <v-btn>Button</v-btn>
+        <v-combobox
+          label="Combobox"
+          :items="['One', 'Two', 'Three']"
+        ></v-combobox>
+      </v-col>
+      <v-col cols="8" class="bg-grey-lighten-3">
+        <div class="align-stretch flex-grow-1" id="openseadragon">Content</div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import OpenSeadragon from "openseadragon";
+
+export default {
+  data: () => ({}),
+  mounted() {
+    let options = {
+      id: "openseadragon",
+      prefixUrl:
+        "https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/",
+      tileSources:
+        "https://iiif.saw-leipzig.de/iiif/2/kucha%2Fimages%2F26823.png/info.json",
+      showRotationControl: true,
+      showFlipControl: true,
+      maxZoomLevel: 100,
+      visibilityRatio: 1.0,
+      defaultZoomLevel: 0,
+      minZoomLevel: 0.001,
+      loadTilesWithAjax: true,
+    };
+    options["showNavigationControl"] = true;
+    this.imageViewer = OpenSeadragon(options);
+  },
+};
+</script>
