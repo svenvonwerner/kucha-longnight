7069772fc68094e4bd7a70bb3433553f09d9ca45
Sven von Werner <sven.von.werner@gmail.com>
2023-05-29 21:13:50 +0200

    add Annotoriouss

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -2,13 +2,15 @@
   <v-container fluid class="bg-grey-lighten-1 fill-height pa-4">
     <v-row no-gutters class="fill-height">
       <v-col cols="4" class="bg-grey-lighten-4">
-        <v-btn>Button</v-btn>
         <v-combobox
-          label="Combobox"
-          :items="['One', 'Two', 'Three']"
-        ></v-combobox>
+          v-model="selectedGame"
+          :items="gameData"
+          :return-object="true"
+          label="Level wählen"
+        >
+        </v-combobox>
       </v-col>
-      <v-col cols="8" class="bg-grey-lighten-3">
+      <v-col cols="8" class="d-flex bg-grey-lighten-3">
         <div class="align-stretch flex-grow-1" id="openseadragon">Content</div>
       </v-col>
     </v-row>
@@ -18,16 +20,27 @@
 <script>
 import OpenSeadragon from "openseadragon";
 import { getGames } from "@/services/repository";
+import Annotorious from "../../static/openseadragon-annotorious.min.js";
+import SelectorPack from "../../static/annotorious-selector-pack.min.js";
+import "../../static/annotorious.min.css";
 
 export default {
-  data: () => ({}),
+  data: () => ({
+    data() {
+      return {
+        imageViewer: null,
+        annotoriousplugin: null,
+        gameData: [],
+        selectedGame: null,
+        selectedLevel: null,
+      };
+    },
+  }),
   mounted() {
     let options = {
       id: "openseadragon",
       prefixUrl:
         "https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/",
-      tileSources:
-        "https://iiif.saw-leipzig.de/iiif/2/kucha%2Fimages%2F26823.png/info.json",
       showRotationControl: true,
       showFlipControl: true,
       maxZoomLevel: 100,
@@ -38,13 +51,73 @@ export default {
     };
     options["showNavigationControl"] = true;
     this.imageViewer = OpenSeadragon(options);
+    var MyHighlightFormatter = function (annotation) {
+      if (annotation.motivation === "highlight") {
+        return {
+          style:
+            "stroke-width:5px;stroke: gold; fill: rgba(0, 128, 0,0.55);vector-effect: none;cursor: auto;",
+        };
+      } else {
+        return {
+          style:
+            "stroke-width:0px;stroke: gold; fill: rgba(0, 128, 0,0);vector-effect: none;cursor: auto;",
+        };
+      }
+    };
+    var config = {};
+    config["readOnly"] = true;
+    (config["disableSelect"] = true),
+      (config["disableEditor"] = true),
+      (config["widgets"] = [
+        "COMMENT",
+        { widget: "TAG", vocabulary: [], showDelete: false },
+      ]);
+    config["image"] = this.imageViewer;
+    config["formatter"] = [MyHighlightFormatter];
+    this.annotoriousplugin = Annotorious(this.imageViewer, config);
+    SelectorPack(this.annotoriousplugin);
+    var _self = this;
+    this.imageViewer.setControlsEnabled(true);
+    this.imageViewer.setMouseNavEnabled(true);
+    //this.annotoriousplugin.disableEditor = true
+    this.annotoriousplugin.on("clickAnnotation", function (annotation, evt) {
+      if ((annotation.motivation = "hide")) {
+        annotation.motivation = "highlight";
+      }
+      _self.annotoriousplugin.format(annotation);
+    });
+
     getGames()
       .then((res) => {
-        console.log("Games are:", res);
+        this.gameData = [];
+        for (var level of res.data) {
+          var newLevel = {
+            text: "Level " + level.gameID,
+            title: "Level " + level.gameID,
+            value: level,
+          };
+          this.gameData.push(newLevel);
+        }
       })
       .catch((err) => {
-        console.log("error!", err.response);
+        console.log("error!", err);
       });
+  },
+  watch: {
+    selectedGame(newVal, oldVal) {
+      console.log("new game selected", newVal.value);
+      this.annotoriousplugin.clearAnnotations();
+      this.imageViewer.open(
+        "https://iiif.saw-leipzig.de/iiif/2/kucha%2Fimages%2F" +
+          newVal.value.imageID +
+          "/info.json"
+      );
+
+      for (let anno of newVal.value.annotations) {
+        anno["motivation"] = "hide";
+        this.annotoriousplugin.addAnnotation(JSON.parse(anno.w3c));
+      }
+    },
   },
 };
 </script>
