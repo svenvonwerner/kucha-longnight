619c919ba09a9e2002557e19c02dd8205e5ba9a6
Sven von Werner <sven.von.werner@gmail.com>
2023-06-01 20:09:40 +0200

    add store.js etc

---
+++ src/store.js
@@ -0,0 +1,22 @@
+import { createStore } from "vuex";
+
+import createPersistedState from "vuex-persistedstate";
+
+export default createStore({
+  state: {
+    statistics: {},
+  },
+  mutations: {
+    setStatistics(state, payload) {
+      console.log("Hello");
+      state.statistics = payload;
+    },
+  },
+  getters: {
+    getStatistics: (state) => {
+      return state.statistics;
+    },
+  },
+  actions: {},
+  plugins: [createPersistedState()],
+});
