6dc0ccf7de138a9144021cd5f546bb2e6f5663d2
Sven von Werner <sven.von.werner@gmail.com>
2023-06-02 17:19:43 +0200

    add store

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,4 +1,6 @@
 <template>
+  <div class="bg-transparent text-h1 text-center" v-html="getTitle"></div>
+
   <v-container fluid class="bg-grey-lighten-1 fill-height pa-4">
     <v-row no-gutters class="fill-height">
       <v-col cols="4" class="bg-grey-lighten-4">
@@ -8,7 +10,13 @@
           :return-object="true"
           label="Level wählen"
         >
+          <template v-slot:append>
+            <v-icon @click="clear()">mdi-restart</v-icon>
+          </template>
         </v-combobox>
+        <v-card-text>
+          {{ selectedGame === null ? "" : selectedGame.value.description }}
+        </v-card-text>
       </v-col>
       <v-col cols="8" class="d-flex bg-grey-lighten-3">
         <div class="align-stretch flex-grow-1" id="openseadragon">Content</div>
@@ -88,6 +96,8 @@ export default {
 
     getGames()
       .then((res) => {
+        console.log("data recieved:", res);
+        console.log("statistics stored:", this.statistics);
         this.gameData = [];
         for (var level of res.data) {
           var newLevel = {
@@ -95,6 +105,26 @@ export default {
             title: "Level " + level.gameID,
             value: level,
           };
+          let annotations = [];
+          let annotationIDs = [];
+          for (let anno of level.annotations) {
+            if (!annotationIDs.includes(anno.annotoriousID)) {
+              annotationIDs.push(anno.annotoriousID);
+              annotations.push(anno);
+            }
+          }
+          level.annotations = annotations;
+          console.log("annos after kicking out doubles: ", annotationIDs);
+          if (!this.statistics[newLevel.value.gameID]) {
+            this.statistics[newLevel.value.gameID] = {
+              fails: 0,
+              number: newLevel.value.annotations.length,
+              annotations: [],
+            };
+          } else {
+            this.statistics[newLevel.value.gameID].number =
+              newLevel.value.annotations.length;
+          }
           this.gameData.push(newLevel);
         }
       })
@@ -105,17 +135,67 @@ export default {
   watch: {
     selectedGame(newVal, oldVal) {
       console.log("new game selected", newVal.value);
-      this.annotoriousplugin.clearAnnotations();
-      this.imageViewer.open(
-        "https://iiif.saw-leipzig.de/iiif/2/kucha%2Fimages%2F" +
-          newVal.value.imageID +
-          "/info.json"
-      );
-
-      for (let anno of newVal.value.annotations) {
-        anno["motivation"] = "hide";
-        this.annotoriousplugin.addAnnotation(JSON.parse(anno.w3c));
+      this.loadLevel(newVal);
+    },
+  },
+  computed: {
+    getTitle() {
+      if (this.selectedGame === null) {
+        return "Finde ... in den Kucha-Gemälden";
+      } else {
+        return (
+          "Finde <span style='color:red;''>" +
+          this.selectedGame.value.name +
+          "</span> in den Kucha-Gemälden!"
+        );
       }
+    },
+    statistics: {
+      get: function () {
+        return this.$store.state.statistics;
+      },
+      set: function (newValue) {
+        console.log("updating");
+        this.$store.commit("setStatistics", newValue);
+      },
+    },
+  },
+  methods: {
+    loadLevel(newLevel) {
+      if (this.loadLevel) {
+        this.annotoriousplugin.clearAnnotations();
+        this.imageViewer.open(
+          "https://iiif.saw-leipzig.de/iiif/2/kucha%2Fimages%2F" +
+            newLevel.value.imageID +
+            "/info.json"
+        );
+        for (let anno of newLevel.value.annotations) {
+          console.log("anno", anno.annotoriousID);
+          let w3cAnno = JSON.parse(anno.w3c);
+          if (
+            this.statistics[newLevel.value.gameID].annotations.includes(
+              anno.annotoriousID
+            )
+          ) {
+            console.log("anno ", anno.annotoriousID, " was already found!");
+            w3cAnno["motivation"] = "highlight";
+          } else {
+            w3cAnno["motivation"] = "hide";
+          }
+          this.annotoriousplugin.addAnnotation(w3cAnno);
+        }
+      }
+    },
+    clear() {
+      this.statistics = {};
+      for (let level of this.gameData) {
+        this.statistics[level.value.gameID] = {
+          fails: 0,
+          number: level.value.annotations.length,
+          annotations: [],
+        };
+      }
+      this.loadLevel(this.selectedGame);
     },
   },
 };
